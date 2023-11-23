55a9c6a50377379ddf9b950e5741064e61b98cd5
akif171 <akifali229@gmail.com>
2023-11-23 11:55:41 +0500

    Note component added

--- src/pages/Candidate/SubPages/MarketingNotes.js
+++ src/pages/Candidate/SubPages/MarketingNotes.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import Note from "../../../components/Note";
 
 const MarketingNotes = () => {
   return (
-    <div>MarketingNotes</div>
-  )
-}
+    <div className="">
+      <div className="w-full   px-6 flex-col justify-start items-start gap-6 inline-flex font-normal mt-5">
+        <div className="flex justify-end items-start gap-6 w-full">
+          <div className="py-2 px-6 bg-teal-600 rounded-full justify-center items-center gap-2 flex">
+            <span className="text-2xl text-white">+</span>
+            <button className="text-white text-base font-normal leading-normal">
+              Add Note
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 w-full ">
 
-export default MarketingNotes
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        <Note />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MarketingNotes;
