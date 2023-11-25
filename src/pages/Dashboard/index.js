a34321e29807142390012d80d16c558f129f62fc
akif171 <akifali229@gmail.com>
2023-11-25 11:47:29 +0500

    dashboard page complete

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import CircleProgressBar from "../../components/CircleProgressBar";
+import ProgressBar from "../../components/ProgressBar";
+import RangeSelector from "../../components/RangeSelector";
 
 const Dashboard = () => {
   const percentage = 40;
@@ -144,18 +146,77 @@ const Dashboard = () => {
         </div>
         <div className="border border-1 p-3 rounded-lg flex flex-col justify-between ">
           <h5>Jobs By Sales Manager </h5>
-          <div className="w-full flex justify-center">
-            <CircleProgressBar percentage={75} color="text-green-500" />
+          <div className="w-full flex flex-col justify-between gap-6">
+            <div className="w-full flex flex-col gap-3">
+              <div className="flex justify-between text-xs">
+                <label for="basic-range-slider-usage" className="">
+                  Description
+                </label>
+                <p className="text-text-hint">Description</p>
+              </div>
+              <input
+                type="range"
+                className="w-full border-none outline-none rounded-full overflow-hidden cursor-pointer  disabled:opacity-50 disabled:pointer-events-none focus:outline-none"
+                id="basic-range-slider-usage"
+                // className="appearance-none bg-transparent [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:bg-black/25 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-[50px] [&::-webkit-slider-thumb]:w-[50px] [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-purple-500"
+              />
+            </div>
+            <div className="w-full flex flex-col gap-3">
+              <div className="flex justify-between text-xs">
+                <label for="basic-range-slider-usage" className="">
+                  Description
+                </label>
+                <p className="text-text-hint">Description</p>
+              </div>
+              <input
+                type="range"
+                className="w-full border-none outline-none rounded-full overflow-hidden cursor-pointer  disabled:opacity-50 disabled:pointer-events-none focus:outline-none"
+                id="basic-range-slider-usage"
+                // className="appearance-none bg-transparent [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:bg-black/25 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-[50px] [&::-webkit-slider-thumb]:w-[50px] [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-purple-500"
+              />
+            </div>
+            <div className="w-full flex flex-col gap-3">
+              <div className="flex justify-between text-xs">
+                <label for="basic-range-slider-usage" className="">
+                  Description
+                </label>
+                <p className="text-text-hint">Description</p>
+              </div>
+              <input
+                type="range"
+                className="w-full border-none outline-none rounded-full overflow-hidden cursor-pointer  disabled:opacity-50 disabled:pointer-events-none focus:outline-none"
+                id="basic-range-slider-usage"
+                // className="appearance-none bg-transparent [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:bg-black/25 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-[50px] [&::-webkit-slider-thumb]:w-[50px] [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-purple-500"
+              />
+            </div>
+            <div className="w-full flex flex-col gap-3">
+              <div className="flex justify-between text-xs">
+                <label for="basic-range-slider-usage" className="">
+                  Description
+                </label>
+                <p className="text-text-hint">Description</p>
+              </div>
+              <input
+                type="range"
+                className="w-full border-none outline-none rounded-full overflow-hidden cursor-pointer  disabled:opacity-50 disabled:pointer-events-none focus:outline-none"
+                id="basic-range-slider-usage"
+                // className="appearance-none bg-transparent [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:bg-black/25 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-[50px] [&::-webkit-slider-thumb]:w-[50px] [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-purple-500"
+              />
+            </div>
           </div>
-          <div className="border border-1 p-3 rounded-lg">
-            <h3>Description</h3>
-            <p className="text-sm">Description</p>
+          <div className="flex justify-between items-center p-3">
+            <h3 className="text-black text-4xl font-normal font-['Inter'] tracking-tight">
+              Data
+            </h3>
+            <button className="text-white text-base font-normal font-['Inter'] tracking-tight bg-secondary-500 py-3 px-6 rounded-lg ">
+              Action
+            </button>
           </div>
         </div>
-        <div className="border border-1 p-3 rounded-lg flex flex-col justify-between ">
+        <div className="border border-1 p-3 rounded-lg flex flex-col justify-between gap-3">
           <h5>Jobs By Sales Manager </h5>
-          <div className="w-full flex justify-center">
-            
+          <div className="w-full  flex justify-center">
+            <ProgressBar percentage={57} />
           </div>
           <div className="border border-1 p-3 rounded-lg">
             <h3>Description</h3>
